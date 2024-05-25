d5fe5fd68aef46bb3f24ada9753c88b2a8d12adf
Aksel Kristoffersen <56234786+aksekris@users.noreply.github.com>
2024-05-25 23:30:56 +0200

    Try remove path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
     import { base } from '$app/paths';
+    console.log(base);
 </script>
 
 <nav>
 	<a href="{base}/">Home</a>
-	<a href="/tutorials">Tutorials</a>
+	<a href="{base}/tutorials">Tutorials</a>
 </nav>
 
 <slot />
