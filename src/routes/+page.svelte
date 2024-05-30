15e65d575274a9bd1203639ef23f1e484657e45f
Aksel Kristoffersen <56234786+aksekris@users.noreply.github.com>
2024-05-30 20:05:40 +0200

    Add stars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
+<script>
+    import Snow from './Snow.svelte'
+</script>
+
 <div class="wrapper">
+    <div class="snow">
+        <Snow></Snow>
+    </div>
     <div class="first-content">
         <h5 class="company-name">Akspertise AS<h5>
         <h1>Aksel Kristoffersen</h1>
@@ -6,39 +13,67 @@
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aspernatur totam atque cum voluptas cumque beatae, porro natus. Commodi, repellendus?</p>
     </div>
     <div class="second-content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque distinctio sequi asperiores nihil voluptatem non, harum perferendis exercitationem, vero quibusdam nemo. Quam enim delectus ut architecto? Maiores corrupti consequuntur ipsa cumque deleniti ab vel nemo ullam quibusdam? Animi nemo, quod hic unde vel corrupti non sunt consequatur esse aliquam ipsum aperiam amet ipsa labore, suscipit dolorum possimus facere repellendus laboriosam est corporis et accusantium? Cumque, voluptas. Esse dolore alias minus impedit, incidunt excepturi molestias fuga. Non facilis atque, exercitationem voluptatem odio sint. Tempore soluta in, perspiciatis, voluptas laudantium quas illo iusto officia aperiam consequuntur nostrum laborum qui dicta vero tempora. Ipsa cumque corrupti quia, accusantium et sint dicta quo assumenda! Voluptatibus omnis quod expedita totam, dolor, maiores, repellat perspiciatis eligendi officia dolore quos alias earum officiis? Error culpa praesentium laborum sed accusantium, sunt doloremque corrupti nisi ipsam placeat temporibus quasi quo rerum minima quaerat dicta ullam! Dolore deleniti vel cupiditate ducimus consectetur necessitatibus hic illo in? Ducimus eum vero consectetur, voluptates id inventore debitis cumque a. Suscipit recusandae animi asperiores repellendus voluptas eaque neque dignissimos, exercitationem sed doloribus blanditiis. Qui quasi ipsa accusantium tempora, error impedit quisquam sequi voluptatum ex quas expedita cupiditate perferendis ipsum, amet excepturi? Vitae, minus asperiores.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit excepturi porro unde veritatis exercitationem recusandae. Modi dolor earum eius laudantium sapiente impedit ipsum voluptates. Iusto veniam sint aliquid reprehenderit sunt itaque, adipisci commodi iste neque possimus ratione laborum corrupti alias velit asperiores, impedit enim doloribus veritatis facere tempora quas molestias est. Ex ea illum natus quas nisi aperiam! Commodi animi itaque voluptatum, laboriosam omnis velit aspernatur, dolorum nemo provident explicabo atque. Quis, ab unde quam, sequi nobis debitis, dignissimos facilis voluptates nemo quibusdam sunt minima illum laboriosam velit iure vel eos? Quibusdam voluptatum quisquam quas ea error nobis deserunt ad dolore, rerum ipsam nostrum cumque architecto, placeat laudantium! Deserunt dicta accusamus alias, modi eos doloremque libero obcaecati nihil consequatur nam ea quisquam ut fugit. Animi repellendus labore, veniam illo veritatis laudantium voluptas quae quo distinctio magni dolorem aspernatur consectetur ea saepe, dolore nobis ratione nihil ullam? Quasi, aspernatur impedit deleniti, voluptates eius fuga neque commodi alias quaerat aliquam in aliquid repellendus sequi provident error iusto itaque vitae doloribus inventore, esse voluptatibus quibusdam! Ratione quisquam modi consectetur porro incidunt commodi ducimus aperiam dolorum est, nihil iusto enim vel quasi ad architecto repellat odio ullam omnis id nemo cumque excepturi. Consectetur sed cum numquam doloribus sequi, laboriosam inventore odit vero eos rerum praesentium quisquam officia eligendi facilis fugiat dolor qui delectus consequatur maiores iste aspernatur. Ad reiciendis amet veniam suscipit recusandae eos minus maiores, eius ducimus impedit quidem mollitia expedita autem assumenda reprehenderit odit at illum eligendi facilis minima! Optio impedit ipsa aperiam, illo aliquam expedita voluptates assumenda nisi, quos voluptatem sapiente enim! In perferendis facere deserunt maiores soluta quas voluptatem quasi sint quaerat sit rerum tenetur laudantium qui, ducimus at deleniti! Molestias ducimus earum soluta excepturi, dolores fuga officia tempora iste officiis animi omnis rerum in. Reprehenderit, pariatur? Voluptate, adipisci, atque in beatae quia et enim placeat, debitis officia amet quos quasi ab recusandae minus obcaecati accusantium voluptatem reprehenderit. Adipisci fugit in unde autem quia ipsum sint perferendis magni aliquam perspiciatis ullam, alias neque illum maxime, id est quos nihil ipsam veniam pariatur eum doloribus. Voluptatum blanditiis cupiditate aspernatur maiores. Culpa quo repudiandae ea temporibus. Praesentium, aut qui tempore accusamus nobis quidem quod assumenda aspernatur ad blanditiis autem officiis expedita eos velit repellendus laboriosam excepturi! Maxime similique unde quas fugit voluptate molestias exercitationem animi beatae vitae, dolorum quo corrupti neque suscipit aliquid, ipsa qui dolorem molestiae enim aperiam autem possimus. In maxime illum mollitia sit atque obcaecati cumque natus neque asperiores, commodi temporibus doloribus, provident facilis velit saepe sint modi ab quia, laudantium recusandae odio aliquam culpa. Voluptas obcaecati quae eos dignissimos incidunt eius repellat magni nobis aliquid adipisci animi rerum beatae quaerat, dolore impedit sequi sit, non iure quia. Placeat est corrupti qui amet totam consequuntur consectetur deserunt ea, excepturi explicabo necessitatibus, quam aperiam quo! Debitis, consequatur at dolore quaerat expedita doloremque perferendis ipsum in eos quam quidem ab odio vero a saepe ullam, earum quae eveniet excepturi? Quis nemo recusandae quae harum consequuntur qui fugiat laborum, quibusdam impedit libero voluptatibus id reiciendis magnam accusamus? Quisquam rerum sint doloremque sapiente totam maiores nostrum praesentium qui, placeat nihil? Exercitationem fugiat distinctio accusamus. Enim et, labore omnis rerum porro quisquam ipsum iusto incidunt illum cumque ex eaque ut deleniti maiores, quo rem adipisci. Molestias, eius? Consequuntur doloremque velit iste laudantium! Voluptatum molestiae est id dolorum excepturi ea deserunt minima distinctio architecto. Nesciunt corrupti saepe magnam, nemo ipsa adipisci, placeat repellendus eligendi aliquam quas veritatis. Ad atque voluptates natus corrupti perspiciatis expedita error magni, consequuntur laborum provident? Nesciunt nostrum cumque cum perferendis tempora cupiditate esse totam, ullam voluptas facere accusantium reprehenderit eligendi. Facilis possimus voluptatibus necessitatibus culpa mollitia debitis. Eligendi sed dolor dignissimos reprehenderit consequatur assumenda tempora perspiciatis illo fuga, ullam amet numquam distinctio? Nostrum dignissimos provident voluptatum impedit dicta reprehenderit quibusdam eveniet, consectetur atque, mollitia porro rem iure ratione maiores saepe voluptate? Fugiat quibusdam quaerat facilis voluptatem ad nulla blanditiis molestias quasi atque tenetur eius sequi hic neque ullam, repellendus voluptatum? Optio sequi magnam animi soluta assumenda! Eligendi fugiat possimus suscipit, voluptates illum dolores nemo ab. Blanditiis illum perspiciatis eius obcaecati minima ab nostrum eum eveniet. Sunt nulla porro deleniti pariatur incidunt aspernatur, eos facilis fuga expedita voluptate eum saepe vitae aliquid sapiente quae nam cupiditate ipsa maxime aut, est quibusdam. Quis, officia quo id temporibus voluptatum sint facilis odio, quam molestiae deleniti laboriosam, consectetur eveniet ratione libero. Ducimus perspiciatis maxime officia placeat ut eius veritatis accusantium harum! Rerum aliquid, suscipit alias necessitatibus voluptates, praesentium deserunt molestias repellendus aut consequuntur sapiente aliquam dignissimos! Eos esse assumenda, numquam enim neque vel labore quidem maxime odit ipsum soluta laborum nulla veniam officia delectus temporibus ea cupiditate voluptatem eligendi quaerat nemo, possimus quam. Voluptates voluptatem eaque eveniet praesentium quisquam, porro quaerat nemo voluptate amet accusantium qui cum at magni atque maxime, exercitationem dolorem officia? Repellat cumque id architecto, consectetur consequatur commodi voluptates nesciunt veniam nemo? Officia modi quae aliquam voluptate nihil dolore ullam illum mollitia officiis magnam error, deserunt nemo odio quos temporibus est voluptas suscipit expedita veniam. Non perferendis atque nemo quas, impedit in eveniet maiores. Architecto dicta ad autem libero animi vel ipsum tempore quidem doloribus facilis, quisquam delectus aliquam et quia, suscipit sequi fugiat voluptate explicabo quo aut molestiae maxime odio numquam perspiciatis! Blanditiis nemo mollitia natus impedit perferendis vero vitae porro dignissimos accusamus, culpa laborum, incidunt ipsum cupiditate. Nesciunt blanditiis placeat reprehenderit libero, nam cum deleniti dignissimos iure enim sed aut. Veniam dolorum quae dolore repellat dicta at recusandae odit, quaerat quos eaque cupiditate eligendi voluptatibus, deleniti repellendus ipsa consequuntur ipsum corporis impedit cumque nam in magnam voluptates? Numquam nobis vel dolores accusamus nihil dicta cupiditate eaque obcaecati eos laudantium sed, error doloribus, soluta assumenda dolor reprehenderit, non expedita! Quam quidem fugit quisquam, laboriosam perferendis magnam dignissimos impedit laborum ab dolorum libero, illo perspiciatis saepe, numquam alias odit! Eligendi adipisci, asperiores praesentium hic ipsa ducimus optio, nam nemo repudiandae expedita molestias? Vitae ducimus eos obcaecati quidem soluta dolorem inventore explicabo adipisci eligendi, expedita sunt natus repellat distinctio voluptas possimus accusantium reprehenderit?</p>
     </div>
 </div>
 
 <style lang="scss">
     .wrapper {
         width: 100%;
-        max-width: 1200px;
+        height: 100vh;
         padding: 30px 20px 30px;
-
+        display: flex;
+        flex-wrap: wrap;
+        overflow-y: auto;
+        overflow-x: hidden;
         @include breakpoint.up('md') {
             padding: 40px 30px 30px;
         }
         @include breakpoint.up('lg') {
-            display: flex;
+            padding: 0px 0px 0px;
+            justify-content: center;
+            flex-direction: row;
             margin: auto;
         }
-    }
-    .first-content {
-        width: 100%;
-        @include breakpoint.up('lg') {
-            padding: 40px 30px 30px;
-            width: 50%;
-        }
-    }
-    .second-content {
-        width: 100%;
-        @include breakpoint.up('lg') {
-            padding: 40px 30px 30px;
-            width: 50%;
-        }
-    }
 
+        .snow {
+            position: relative;
+            height: 70px;
+            width: 100%;
+            z-index: -1;
+            overflow: hidden;
+            @include breakpoint.up('lg') {
+                top: 0px;
+                position: sticky;
+                position: -webkit-sticky;
+                z-index: -1;
+            }
+            filter: drop-shadow(0 0 10px white);
+        }
+        .first-content {
+            width: 100%;
+            top: 0px;
+            position: relative;
+            @include breakpoint.up('lg') {
+                width: 50%;
+                max-width: 600px;
+                padding: 40px 30px 30px 60px;
+                top: 70px;
+                position: sticky;
+                position: -webkit-sticky;
+                height: fit-content;
+            }
+        }
+        .second-content {
+            width: 100%;
+            @include breakpoint.up('lg') {
+                width: 50%;
+                max-width: 600px;
+                padding: 40px 50px 30px 0px;
+            }
+        }
+        
+    }
 
     h1 {
         margin-top: -13px;
