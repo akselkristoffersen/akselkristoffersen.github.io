b68152c99181e01b99e2abca44157b75cba8469a
Aksel Kristoffersen <56234786+aksekris@users.noreply.github.com>
2024-05-31 19:14:22 +0200

    Change height of stars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,7 +69,7 @@
 
         .stars {
             position: relative;
-            height: 100px;
+            height: 220px;
             width: 100%;
             z-index: -1;
             @include breakpoint.up('lg') {
