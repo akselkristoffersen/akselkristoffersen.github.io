7a2346d7fe6d9cb3ed6d8eea11f43062ba6a3421
Aksel Kristoffersen <56234786+aksekris@users.noreply.github.com>
2024-07-31 00:13:48 +0200

    Thicker svg resume

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -148,7 +148,7 @@
                     target="_blank" rel="noopener noreferrer" 
                     title="Resume"
                     >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="2 0 24 24" fill="none" stroke="currentColor" stroke-width="2.25" stroke-linecap="square" stroke-linejoin="bevel" class="lucide lucide-file-text"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>            
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="2 0 24 24" fill="none" stroke="currentColor" stroke-width="2.6" stroke-linecap="square" stroke-linejoin="bevel" class="lucide lucide-file-text"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>            
             </div>
             <div class="hero-blog-posts">
                 <h2>BLOG POSTS:</h2>
