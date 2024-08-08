0f8d371f79f043f4a506cea72f6d482d700e3835
Aksel Kristoffersen <56234786+aksekris@users.noreply.github.com>
2024-08-08 15:28:46 +0200

    Update github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -138,7 +138,7 @@
                     >
                     <svg class="svg-fill" viewBox="0.9119999999999999 3.6959999999999997 22.128 13.424" xmlns="http://www.w3.org/2000/svg" style="max-height: 500px" width="22.728" height="21.754"><path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/></svg>
                 </a>
-                <a href="https://github.com/akspertise"
+                <a href="https://github.com/akselkristoffersen"
                     target="_blank" rel="noopener noreferrer" 
                     aria-label="Github (opens in a new tab)" title="Github"
                     >
